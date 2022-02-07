2c853bfcc6b5409a35a5f1a01454a57be72c1607
RaviTeja6699 <vaggaraviteja63@gmail.com>
2022-02-07 21:53:20 +0530

    3 files modified live score

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('hello!')
+var runs=0;
+var wicket=0;
+var livescore=runs+'/'+wicket;
+function score(val){
+  if (val==='w'){
+    wicket++;
+    var livescore=runs+'/'+wicket;
+    document.getElementById('d1').innerHTML=livescore;
+    
+  }
+  else{
+    runs=runs+val;
+    var livescore=runs+'/'+wicket;
+    document.getElementById('d1').innerHTML=livescore;
+    
+
+
+  }
+}
